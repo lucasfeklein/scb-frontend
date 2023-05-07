d90a781adf0c093a0c53019a75f7cdef6219a7e2
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-07 16:21:17 -0300

    chat image added

--- components/About/AboutSectionOne.tsx
+++ components/About/AboutSectionOne.tsx
@@ -50,11 +50,11 @@ const AboutSectionOne = () => {
 
             <div className="w-full px-4 lg:w-1/2">
               <div
-                className="wow fadeInUp relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0"
+                className="wow fadeInUp relative mx-auto aspect-[20/24] max-w-[500px] lg:mr-0"
                 data-wow-delay=".2s"
               >
                 <Image
-                  src="/images/about/about-image.svg"
+                  src="/images/about/chat.png"
                   alt="about-image"
                   fill
                   className="mx-auto max-w-full lg:mr-0"
