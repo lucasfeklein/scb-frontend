ee4b07c564d614fade553c18e64c865a8be5da9c
Rafael Muttoni <rafael@muttoni.com.br>
2023-05-12 12:02:26 +0700

    api

--- pages/dashboard/create.tsx
+++ pages/dashboard/create.tsx
@@ -1,39 +1,69 @@
-import DashboardLayout from '@/components/Dashboard/DashboardLayout';
-import { useState } from 'react';
+import DashboardLayout from "@/components/Dashboard/DashboardLayout";
+import { api } from "@/services/api";
+import { useState } from "react";
 
 function CreateChatbot() {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
 
   const handleUrlChange = (event) => {
     setUrl(event.target.value);
-  }
+  };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Do something with the url value, such as submitting a form
-    setUrl('')
-    console.log(url);
-  }
+
+    const token = localStorage.getItem("token");
+
+    try {
+      const { data } = await api.post(
+        "/company",
+        { name: url, website: url },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setUrl("");
+  };
 
   return (
     <DashboardLayout>
-      <div className="flex flex-col items-center justify-center h-full">
-        <h2 className="text-xl font-bold mb-4">Digite sua URL</h2>
+      <div className="flex h-full flex-col items-center justify-center">
+        <h2 className="mb-4 text-xl font-bold">Digite sua URL</h2>
         <form onSubmit={handleSubmit} className="flex items-center">
           <input
             type="text"
             value={url}
             onChange={handleUrlChange}
             placeholder="https://example.com/"
-            className="px-4 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-primary"
+            className="rounded-l-md border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
           />
           <button
             type="submit"
-            className="px-4 py-2 bg-primary text-white rounded-r-md"
+            className="rounded-r-md bg-primary px-4 py-2 text-white"
           >
             Enviar
           </button>
         </form>
+
+        <input
+          type="text"
+          value={"https://pub-2517a1c0fa894c8a94a8080903a74481.r2.dev/chat.js"}
+          className="rounded-l-md border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-primary"
+          onClick={async () => {
+            await navigator.clipboard.writeText(
+              "https://pub-2517a1c0fa894c8a94a8080903a74481.r2.dev/chat.js"
+            );
+
+            alert("copied url");
+          }}
+        />
       </div>
     </DashboardLayout>
   );
--- pages/magic-link/index.tsx
+++ pages/magic-link/index.tsx
@@ -1,42 +1,29 @@
+import { api } from "@/services/api";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const MagicLink = () => {
-    const router = useRouter();
-    const token = router.query.token;
+  const router = useRouter();
+  const token = router.query.token;
 
-    useEffect(() => {
-        const verifyMagicLink = async () => {
-            try {
-                const requestOptions = {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ token }),
-                };
-                const response = await fetch(
-                    "http://localhost:8000/verify-magic-link",
-                    requestOptions
-                );
-                if (response.ok) {
-                    // handle success response
-                    const data = await response.json()
-                    localStorage.setItem("token", data.token);
-                    router.push('/dashboard')
-                } else {
-                    // handle error response
-                }
-            } catch (error) {
-                // handle network error
-            }
-        };
-        token && verifyMagicLink();
-    }, [token]);
+  useEffect(() => {
+    const verifyMagicLink = async () => {
+      try {
+        const { data } = await api.post("/verify-magic-link", { token });
+        localStorage.setItem("token", data.token);
+        router.push("/dashboard");
+      } catch (error) {
+        // handle network error
+      }
+    };
+    token && verifyMagicLink();
+  }, [token]);
 
-    return (
-        <>
-            <h1>Loading...</h1>
-        </>
-    );
+  return (
+    <>
+      <h1>Loading...</h1>
+    </>
+  );
 };
 
 export default MagicLink;
