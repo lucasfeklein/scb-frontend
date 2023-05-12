3d9be8b0d32e08ec8fc5f34cc550877735df50f1
Rafael Muttoni <rafael@muttoni.com.br>
2023-05-12 12:18:58 +0700

    api

--- components/Dashboard/DashboardNavbar.tsx
+++ components/Dashboard/DashboardNavbar.tsx
@@ -1,79 +1,76 @@
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { BsFillPlusCircleFill } from 'react-icons/bs';
-import { FaEdit } from 'react-icons/fa';
-import { MdSpaceDashboard } from 'react-icons/md';
+import { api } from "@/services/api";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { FaEdit } from "react-icons/fa";
+import { MdSpaceDashboard } from "react-icons/md";
 
 function DashboardNavbar() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const token = localStorage.getItem('token');
-        const response = await fetch('http://localhost:8000/auth-verify', {
+        const token = localStorage.getItem("token");
+        const { data } = await api.get("/auth-verify", {
           headers: {
-            'Authorization': `Bearer ${token}`
-          }
+            Authorization: `Bearer ${token}`,
+          },
         });
-        if (!response.ok) {
-          throw new Error('Seu tempo expirou! Favor entrar novamente.');
-        }
-        const data = await response.json();
         setEmail(data.email);
       } catch (error) {
         console.error(error);
         alert(error.message);
-        router.push('/signin');
+        router.push("/signin");
       }
     }
     fetchData();
   }, []);
 
   return (
-    <div className="fixed top-0 left-0 h-screen w-60 z-50">
-      <div className="flex flex-col h-full w-full border-r dashboard-border-color">
-        <div className="py-4 px-6 bg-gray-900 border-b dashboard-border-color">
+    <div className="fixed left-0 top-0 z-50 h-screen w-60">
+      <div className="dashboard-border-color flex h-full w-full flex-col border-r">
+        <div className="bg-gray-900 dashboard-border-color border-b px-6 py-4">
           <h1 className="text-lg font-bold">ChatChima Bot</h1>
         </div>
-        <div className="flex gap-2 items-center py-4 px-6 border-b dashboard-border-color" >
+        <div className="dashboard-border-color flex items-center gap-2 border-b px-6 py-4">
           <MdSpaceDashboard />
           <p>Dashboard</p>
         </div>
-        <div className="flex-1 py-4 px-6 bg-gray">
+        <div className="flex-1 bg-gray px-6 py-4">
           <ul className="space-y-2">
             <li>
               <Link
                 href="/dashboard/create"
-                className={`flex gap-2 items-center py-2 px-6 rounded-md hover:text-white hover:bg-primary ${
-                  router.pathname === '/dashboard/create' ? 'text-primary' : ''
+                className={`flex items-center gap-2 rounded-md px-6 py-2 hover:bg-primary hover:text-white ${
+                  router.pathname === "/dashboard/create" ? "text-primary" : ""
                 }`}
               >
                 <BsFillPlusCircleFill />
-                <p>Criar</p>                
+                <p>Criar</p>
               </Link>
             </li>
             <li>
               <Link
                 href="/dashboard/custom"
-                className={`flex gap-2 items-center py-2 px-6 rounded-md hover:text-white hover:bg-primary ${
-                  router.pathname === '/dashboard/custom' ? 'text-primary' : ''
+                className={`flex items-center gap-2 rounded-md px-6 py-2 hover:bg-primary hover:text-white ${
+                  router.pathname === "/dashboard/custom" ? "text-primary" : ""
                 }`}
               >
-                <FaEdit/>
-                <p>Customizar</p>       
+                <FaEdit />
+                <p>Customizar</p>
               </Link>
             </li>
           </ul>
         </div>
-        <div className="py-4 px-6 border-t dashboard-border-color">
+        <div className="dashboard-border-color border-t px-6 py-4">
           <p>Email: {email}</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default DashboardNavbar;
