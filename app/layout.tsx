2f57f8699d77d19638899e7b02ca176fbf4a6bf3
Lucas Klein <lucas.fklein@hotmail.com>
2023-05-13 23:15:21 -0300

    clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import ScrollToTop from "@/components/ScrollToTop";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 
@@ -24,7 +23,6 @@ export default function RootLayout({
           <Header />
           {children}
           <Footer />
-          <ScrollToTop />
         </Providers>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
@@ -38,7 +37,6 @@ export default function Home() {
 
   return (
     <>
-      <ScrollUp />
       <Hero />
       <Features />
       <AboutSectionOne />
