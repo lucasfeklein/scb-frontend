460a9da6ac69d9c4046be4e35db0a1263af0a436
Rafael Muttoni <rafael@muttoni.com.br>
2023-05-14 10:17:35 +0700

    dashboard

--- components/Dashboard/DashboardNavbar.tsx
+++ components/Dashboard/DashboardNavbar.tsx
@@ -1,10 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { api } from "@/services/api";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { BsFillPlusCircleFill } from "react-icons/bs";
-import { FaEdit } from "react-icons/fa";
-import { MdSpaceDashboard } from "react-icons/md";
 
 function DashboardNavbar() {
   const router = useRouter();
@@ -30,45 +35,49 @@ function DashboardNavbar() {
   }, []);
 
   return (
-    <div className="fixed left-0 top-0 z-50 h-screen w-60">
-      <div className="dashboard-border-color flex h-full w-full flex-col border-r">
-        <div className="bg-gray-900 dashboard-border-color border-b px-6 py-4">
-          <h1 className="text-lg font-bold">ChatChima Bot</h1>
-        </div>
-        <div className="dashboard-border-color flex items-center gap-2 border-b px-6 py-4">
-          <MdSpaceDashboard />
-          <p>Dashboard</p>
-        </div>
-        <div className="flex-1 bg-gray px-6 py-4">
-          <ul className="space-y-2">
-            <li>
-              <Link
-                href="/dashboard/create"
-                className={`flex items-center gap-2 rounded-md px-6 py-2 hover:bg-primary hover:text-white ${
-                  router.pathname === "/dashboard/create" ? "text-primary" : ""
-                }`}
-              >
-                <BsFillPlusCircleFill />
-                <p>Criar</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/dashboard/custom"
-                className={`flex items-center gap-2 rounded-md px-6 py-2 hover:bg-primary hover:text-white ${
-                  router.pathname === "/dashboard/custom" ? "text-primary" : ""
-                }`}
-              >
-                <FaEdit />
-                <p>Customizar</p>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="dashboard-border-color border-t px-6 py-4">
-          <p>Email: {email}</p>
-        </div>
-      </div>
+    <div className="border-b">
+    <div className="flex h-16 items-center px-4">
+
+    <div className="mr-4 font-bold">
+      <h5>
+        Dashboard Chatbot
+      </h5>
+    </div>
+
+    <nav
+      className={"flex items-center space-x-4 lg:space-x-6 mx-6"}
+     
+    >
+      <Link
+           href="/dashboard/create"
+       >
+        Create
+      </Link>
+      
+    </nav>
+
+    <div className="ml-auto flex items-center space-x-4">
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarFallback>{email[0]}{email[1]}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-xs leading-none text-muted-foreground">
+             {email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+      </DropdownMenuContent>
+    </DropdownMenu></div>
+
+    </div>
     </div>
   );
 }
