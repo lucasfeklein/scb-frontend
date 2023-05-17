db30fea36fb6499ee408f16323d64fb74c55fc93
Rafael Muttoni <rafael@muttoni.com.br>
2023-05-17 11:24:30 +0700

    ts

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,10 @@
-declare module "stripe-pricing-table" {
-  // Export the component, replacing `MyComponent` with the actual component's name from 'stripe-pricing-table'
-  export function MyComponent(props: any): JSX.Element;
-}
-
 declare namespace JSX {
   interface IntrinsicElements {
     // Add 'stripe-pricing-table' as a valid intrinsic element for JSX
-    "stripe-pricing-table": any;
+    "stripe-pricing-table": {
+      // Add specific attributes for the 'stripe-pricing-table' custom element
+      "pricing-table-id": string;
+      "publishable-key": string;
+    };
   }
 }
